6b9e9a840d8857541239087e3d90e24e9cbf294a
Luis Enrique Santana Del Pozo <kikesantana52@gmail.com>
2018-02-12 12:34:44 -0600

    index created well and directory

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-var controller = new ScrollMagic.Controller();
+// var controller = new ScrollMagic.Controller();
+//
+// var scene = new ScrollMagic.Scene({
+//   triggerElement: '#bar-2'
+// }).setTween('#semilla',1,{display: 'block'})
+// .addTo(controller);
+//
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: '#bar-2'
-}).setTween('#semilla',1,{display: 'block'})
-.addTo(controller);
+
+$(document).ready(function() {
+
+var flag = false;
+var scroll;
+
+    $(window).scroll(function() {
+       scroll = $(window).scrollTop();
+       if (scroll>1000) {
+         if(!flag){
+           $(".logo").css({"margin-top" : "0","width":"50px","height" : "50px"});
+           flag = true;
+         }
+       }else{
+         if (flag) {
+           $(".logo").css({"margin-top" : "50%","width":"250px","height" : "250px"});
+           flag = false;
+         }
+
+       }
+    });
+});
