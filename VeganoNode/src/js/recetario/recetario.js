71818ef5c99d68a74975b551eaa939da50f7bef3
Luis Enrique Santana Del Pozo <kikesantana52@gmail.com>
2018-02-15 13:46:04 -0600

    Imagenes heredadas de data-imgsetter

--- VeganoNode/src/js/recetario/recetario.js
+++ VeganoNode/src/js/recetario/recetario.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var bool_extendida = false;
 
+  //Presionar el boton de filter para mostrar filtros
   $('#filterBtn').on('click',()=>{
     $('.fa-arrow-up').toggleClass('hide');
     $('.fa-arrow-down').toggleClass('hide');
@@ -13,8 +14,24 @@ $(document).ready(function() {
     }
 
 
-    $('.img-background').css({
-      'background' : 'url("' + $(this).data('data-img') + '" no-repeat cover);'
-    });
+
+  });
+
+  //Efecto de solidificación
+  $('#intro').hover(function() {
+    $('.grey-overlay').css('background','rgba(125,125,125,.9)')
+  },function() {
+    $('.grey-overlay').css('background','rgba(125,125,125,.5)')
+  });
+
+  //Obtener la informacion en 'data-imgsetter' y ponerla como background cover
+  $('div.img-background').each(function(){
+    $(this).css({
+      'background':'url(' + $(this).data('imgsetter') + ')',
+      'background-repeat':'no-repeat',
+      'background-size':'cover',
+      'background-position':'center'
+  });
+
   });
 });
