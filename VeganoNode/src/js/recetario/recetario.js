f5126bdb356c7e313de563f4a1855f02af75304a
KikeOficial <kikesantana52@gmail.com>
2018-02-21 23:47:00 -0600

    Espaciado

--- VeganoNode/src/js/recetario/recetario.js
+++ VeganoNode/src/js/recetario/recetario.js
@@ -2,26 +2,26 @@ $(document).ready(function() {
   var bool_extendida = false;
 
   //Presionar el boton de filter para mostrar filtros
-  $('#filterBtn').on('click',()=>{
-    $('.fa-arrow-up').toggleClass('hide');
-    $('.fa-arrow-down').toggleClass('hide');
-    if (bool_extendida) {
-      $('#intro').css({'height':'120px'});
-      bool_extendida = false;
-    }else{
-      $('#intro').css({'height':'360px'});
-      bool_extendida = true;
-    }
-
-
-
-  });
+  // $('#filterBtn').on('click',()=>{
+  //   $('.fa-arrow-up').toggleClass('hide');
+  //   $('.fa-arrow-down').toggleClass('hide');
+  //   if (bool_extendida) {
+  //     $('#recepies').css({'padding-top':'8%'});
+  //     bool_extendida = false;
+  //   }else{
+  //     $('#recepies').css({'padding-top':'50%'});
+  //     bool_extendida = true;
+  //   }
+  //
+  //
+  //
+  // });
 
   //Efecto de solidificación
   $('#intro').hover(function() {
-    $('.grey-overlay').css('background','rgba(125,125,125,.9)')
+    $('.dark-overlay').css('background','rgba(0,0,0,.9)')
   },function() {
-    $('.grey-overlay').css('background','rgba(125,125,125,.5)')
+    $('.dark-overlay').css('background','rgba(0,0,0,.5)')
   });
 
   //Obtener la informacion en 'data-imgsetter' y ponerla como background cover
