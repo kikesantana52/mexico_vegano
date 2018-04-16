c7b3785a57a34c9bd9fd24e985007c276b7a7765
KikeOficial <kikesantana52@gmail.com>
2018-04-16 18:31:57 -0500

    Cambios generales

--- VeganoNode/DB_mexico_vegano.sql
+++ VeganoNode/DB_mexico_vegano.sql
@@ -4,29 +4,30 @@ CREATE DATABASE Mexico_Vegano;
 USE Mexico_Vegano;
 
 
-CREATE TABLE mex_argumentos{
+CREATE TABLE mex_argumentos(
   id INT(6) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   titulo NVARCHAR(64) NOT NULL,
   contenido TEXT,
   icono NVARCHAR(255)
-}
+);
 
-CREATE TABLE mex_clientes{
+CREATE TABLE mex_clientes(
   id NVARCHAR(64) PRIMARY KEY NOT NULL,
   titulo NVARCHAR(64) NOT NULL,
   calle NVARCHAR(32),
   noext NVARCHAR(32),
   noint NVARCHAR(32),
+  colonia NVARCHAR(32),
+  web NVARCHAR(32),
   telefono NVARCHAR(32),
-  slogan NVARCHAR(255),
-}
+  correo NVARCHAR(32),
+  slogan NVARCHAR(255)
+);
 
 
-CREATE TABLE mex_recetas{
+CREATE TABLE mex_recetas(
   id NVARCHAR(64) PRIMARY KEY NOT NULL,
   titulo NVARCHAR(64) NOT NULL,
   tiempo NVARCHAR(64) NOT NULL,
-  proceso TEXT,
-
-
-}
+  proceso TEXT
+);
